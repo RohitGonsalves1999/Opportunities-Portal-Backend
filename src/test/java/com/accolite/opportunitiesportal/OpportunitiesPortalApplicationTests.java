2e255641d90058c5b0a53a2cd49465f9254c5d8b
RohitGonsalves1999 <32949288+RohitGonsalves1999@users.noreply.github.com>
2020-05-25 16:10:20 +0530

    Initial Commit

---
+++ src/test/java/com/accolite/opportunitiesportal/OpportunitiesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolite.opportunitiesportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportunitiesPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
