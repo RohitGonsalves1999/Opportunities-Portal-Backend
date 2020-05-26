5c67542bfaa80de57c250923232a4407985a3c0f
RohitGonsalves1999 <32949288+RohitGonsalves1999@users.noreply.github.com>
2020-05-26 10:16:37 +0530

    Resolved CORS

--- src/main/java/com/accolite/opportunitiesportal/jobs/controller/JobController.java
+++ src/main/java/com/accolite/opportunitiesportal/jobs/controller/JobController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.accolite.opportunitiesportal.jobs.service.JobsService;
 
 @RestController
 @RequestMapping("/api/jobs")
+
 public class JobController {
 	
 	@Autowired
